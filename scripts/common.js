d38cb3ed2ef5e07ebc445aa05974c1a6b8e4c479
jiangting <jiangting8989@126.com>
2018-09-08 19:00:04 +0800

    保存token

--- scripts/common.js
+++ scripts/common.js
@@ -208,7 +208,9 @@ lazyLoad.require(['https://g.alicdn.com/msui/sm/0.6.2/js/sm.min.js'],function(){
         },
         addEvent:function(){
             var _this = this;
-            if(_this.isXYWL && XYNativeClient && $('#userId').length > 0)XYNativeClient.getUserId('getNativeId');
+            if(_this.isXYWL && XYNativeClient && $('#userId').length > 0){
+                XYNativeClient.getUserId('getNativeId');
+            }
             window.getNativeId = function(id){
                 $('#userId').val(id);
             }
